7e1b32eaaf2fdfdeeed7f4e67ccdf414311432b4
kjayru <wiltinoco@gmail.com>
2023-04-14 15:32:34 -0500

    fix

--- app/Http/Controllers/Backend/HomeController.php
+++ app/Http/Controllers/Backend/HomeController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Register;
-use App\Model\Contact;
+use App\Models\Contact;
 
 class HomeController extends Controller
 {
